9c649b833f60974595bce60a16d0f2c350c91963
Eugene Ware <eugene@noblesamurai.com>
2017-04-11 18:23:01 +1000

    fixes race condition

--- index.js
+++ index.js
@@ -4,11 +4,24 @@ const assert = require('assert')
 module.exports = (pid, cb) => {
   assert(pid, 'pid required')
   const proc = run('osascript', [])
-  proc.end(`
-    tell application "System Events"
-      set frontmost of the first process whose unix id is ${pid} to true
-    end tell
-  `)
+  setImmediate(() => {
+    proc.end(`
+      tell application "System Events"
+        repeat with tries from 1 to 3
+          try
+            set frontmost of the first process whose unix id is ${pid} to true
+            exit repeat
+          on error errText number errNum
+          end try
+          delay 1
+        end repeat
+        if tries = 3 then
+          error errText number errNum
+        end if
+      end tell
+    `)
+  });
   proc.on('error', cb)
   proc.on('end', cb)
 }
+
